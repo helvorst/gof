8488520667b8824de74bb787d7f9f94be3f4a1f7
hel vorst <helvorst@hels-MacBook-Air.local>
2020-06-06 15:11:59 -0700

    init setup-2

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+module.exports = {
+    entry: './src/processor.ts',
+    devtool: 'inline-source-map',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+                include: [
+                    path.resolve(__dirname, "src")
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+        filename: 'processor.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
